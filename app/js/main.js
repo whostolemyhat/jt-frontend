f5c1ebb21a1b4955365ab8bd389a6dc5fe5177c4
James Tease <james.tease@rockpooldigital.com>
2014-06-20 12:44:06 +0100

    nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
+var app = app || {};
+
 $(document).ready(function() {
     console.log('Running jQuery version', $.fn.jquery);
+
+    app.nav.mobileMenu();
 });
+
+app.nav = (function() {
+    function mobileMenu() {
+        $('.nav__mobile-menu').click(function(e) {
+            e.preventDefault();
+
+            $('.nav').toggleClass('nav--active');
+        });
+    }
+
+    return {
+        mobileMenu: mobileMenu
+    };
+})();
